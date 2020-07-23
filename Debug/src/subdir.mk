583ff60141fd96e003bb8566c665273092fa0992
KornevEK <kornev.ek@phystech.edu>
2020-07-24 02:48:16 +0300

    Testing

--- Debug/src/subdir.mk
+++ Debug/src/subdir.mk
@@ -4,13 +4,13 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 CPP_SRCS += \
-../src/mesh.cpp 
+../src/tensor_class.cpp 
 
 OBJS += \
-./src/mesh.o 
+./src/tensor_class.o 
 
 CPP_DEPS += \
-./src/mesh.d 
+./src/tensor_class.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
