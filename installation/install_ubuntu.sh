afeb72f66a23fc62b6818e165cc6839cea9b0559
chikitkin <alexchikitkin@gmail.com>
2022-03-02 09:33:05 +0300

    modify README

---
+++ installation/install_ubuntu.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# 
+wget -O- https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB \
+| gpg --dearmor | sudo tee /usr/share/keyrings/oneapi-archive-keyring.gpg > /dev/null
+
+echo "deb [signed-by=/usr/share/keyrings/oneapi-archive-keyring.gpg] https://apt.repos.intel.com/oneapi all main" | sudo tee /etc/apt/sources.list.d/oneAPI.list
+
+sudo apt-get update
+
+sudo apt install intel-hpckit
